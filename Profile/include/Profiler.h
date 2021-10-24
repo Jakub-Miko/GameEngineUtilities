cb83b05bebf6054d0b432a29bab3e6a38e4b6439
mainm <mainm@TROJAN>
2021-10-24 19:44:33 +0200

    Removed Default Enable Profiling Definition

--- Profile/include/Profiler.h
+++ Profile/include/Profiler.h
@@ -149,7 +149,6 @@ public:
 
 // Macros for easy placement of timers and maintaining sessions, as well as for striping the code from release builds.
 
-#define ENABLE_PROFILING
 #ifdef ENABLE_PROFILING
 	
 		#ifndef COMBINE
