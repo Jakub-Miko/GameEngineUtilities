cf99de45f897b3011ed831611b9db10592ae9d1c
mainm <mainm@TROJAN>
2022-06-19 21:17:28 +0200

    Improved FileManager, added literal operators and loading paths from config.

--- FileManager/FileManager.cpp
+++ FileManager/FileManager.cpp
@@ -1,12 +1,34 @@
 #include "FileManager.h"
+#include "FileManager.h"
+#include "FileManager.h"
 #include <filesystem>
+#include <ConfigManager.h>
+
 
 FileManager* FileManager::instance = nullptr;
 
-void FileManager::Init()
+void FileManager::Init(const FileManager_paths& paths)
 {
 	if (!instance) {
-		instance = new FileManager();
+		instance = new FileManager(paths);
+	}
+}
+
+void FileManager::Init()
+{
+	FileManager_paths paths;
+
+	if (!ConfigManager::Get()) {
+		throw std::runtime_error("If you don't specify explicit file paths, you  need to initialize ConfigManager before FileManager");
+	}
+
+	paths.root_path = FileManager::GetRelativeBinaryPath("/") + ConfigManager::Get()->GetString("root_path");
+	paths.local_asset_path = paths.root_path + ConfigManager::Get()->GetString("local_asset_path");
+	paths.engine_asset_path = paths.root_path + ConfigManager::Get()->GetString("engine_asset_path");
+	paths.render_api_path = paths.root_path + ConfigManager::Get()->GetString("render_api_path");
+
+	if (!instance) {
+		instance = new FileManager(paths);
 	}
 }
 
@@ -22,33 +44,71 @@ FileManager* FileManager::Get()
 	return instance;
 }
 
+std::string FileManager::GetPath(const std::string& path)
+{
+	auto colon = path.find(':', 0);
+	
+	
+	if (colon != path.npos) {
+		auto directive = path.substr(0, colon);
+		auto file_path = path.substr(colon+1);
+		if (std::isupper(directive[0])) {
+			throw std::runtime_error("Invalid path format, first character of directive can't be uppercase. Make sure you are using relative paths");
+		}
+		if (directive == "asset") {
+			return GetAssetFilePath(file_path);
+		}
+		else if(directive == "engine_asset") {
+			return GetEngineAssetFilePath(file_path);
+		}
+		else if (directive == "api") {
+			return GetRenderApiAssetFilePath(file_path);
+		}
+		else {
+			throw std::runtime_error("Unknown directive " + directive);
+		}
+	}
+	else {
+		return GetRootPath() + path;
+	}
+
+
+
+}
+
 std::string FileManager::GetRelativeFilepath(const std::string& path)
 {
-	return GetRootPath() + "/" + path;
+	return GetRootPath() + path;
 }
 
 std::string FileManager::GetRenderApiAssetFilePath(const std::string& path)
 {
-	return GetRootPath() + render_api_assets_filepath + "/" + path;
+	return paths.render_api_path + path;
 }
 
 std::string FileManager::GetAssetFilePath(const std::string& path)
 {
-	return GetRootPath() + "/assets/" + path;
+	return paths.local_asset_path + path;
+}
+
+std::string FileManager::GetEngineAssetFilePath(const std::string& path)
+{
+	return paths.engine_asset_path + path;
 }
 
 std::string FileManager::GetRootPath()
 {
-	return std::filesystem::current_path().parent_path().generic_string();
+	return paths.root_path;
 }
 
-FileManager::FileManager()
+std::string FileManager::GetRelativeBinaryPath(const std::string& path)
 {
-#if RENDER_API == OpenGL
-	render_api_assets_filepath = "/assets/OpenGL";
-#else 
-	render_api_assets_filepath = "/assets";
-#endif
+	return std::filesystem::current_path().generic_string() + path;
+}
+
+FileManager::FileManager(const FileManager_paths& paths) : paths(paths)
+{
+
 }
 
 FileManager::~FileManager()
--- FileManager/FileManager.h
+++ FileManager/FileManager.h
@@ -1,6 +1,13 @@
 #pragma once
 #include <string>
 
+struct FileManager_paths {
+	std::string root_path = "Unknown";
+	std::string local_asset_path = "Unknown";
+	std::string engine_asset_path = "Unknown";
+	std::string render_api_path = "Unknown";
+};
+
 class FileManager {
 public:
 
@@ -9,18 +16,27 @@ public:
 	FileManager& operator=(const FileManager& ref) = delete;
 	FileManager& operator=(FileManager&& ref) = delete;
 
+	static void Init(const FileManager_paths& paths);
 	static void Init();
 	static void Shutdown();
 	static FileManager* Get();
 
+	std::string GetPath(const std::string& path);
 	std::string GetRelativeFilepath(const std::string& path);
 	std::string GetRenderApiAssetFilePath(const std::string& path);
 	std::string GetAssetFilePath(const std::string& path);
+	std::string GetEngineAssetFilePath(const std::string& path);
 	std::string GetRootPath();
+
+	static std::string GetRelativeBinaryPath(const std::string& path);
 
 private:
 	static FileManager* instance;
-	std::string render_api_assets_filepath;
-	FileManager();
+	FileManager_paths paths;
+	FileManager(const FileManager_paths& paths);
 	~FileManager();
 };
+
+inline std::string operator"" _path(const char* in_path, std::size_t) {
+	return FileManager::Get()->GetPath(in_path);
+}
